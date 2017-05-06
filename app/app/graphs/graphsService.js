b163348e161886346919c5a6bd634b1b582b9274
Leticia Wanderley <leticiafwanderley@gmail.com>
2017-05-06 13:01:08 -0300

    Adicionando imagem

--- app/app/graphs/graphsService.js
+++ app/app/graphs/graphsService.js
@@ -4,28 +4,28 @@ var somDoLSD = angular.module('somDoLSD');
 somDoLSD.factory('graphsService', function($http) {
   var graphsService = {
     getTopUsers: function() {
-      var promise = $http.get('http://10.30.0.33:8080/top_users').then(function (response) {
+      var promise = $http.get('http://10.11.4.160:8080/top_users').then(function (response) {
         console.log(response);
         return response.data;
       });
       return promise;
     },
     getTopArtists: function() {
-      var promise = $http.get('http://10.30.0.33:8080/top_artists').then(function (response) {
+      var promise = $http.get('http://10.11.4.160:8080/top_artists').then(function (response) {
         console.log(response);
         return response.data;
       });
       return promise;
     },
     getTopTracks: function() {
-      var promise = $http.get('http://10.30.0.33:8080/top_tracks').then(function (response) {
+      var promise = $http.get('http://10.11.4.160:8080/top_tracks').then(function (response) {
         console.log(response);
         return response.data;
       });
       return promise;
     },
     getTopTags: function() {
-      var promise = $http.get('http://10.30.0.33:8080/top_tags').then(function (response) {
+      var promise = $http.get('http://10.11.4.160:8080/top_tags').then(function (response) {
         console.log(response);
         return response.data;
       });
