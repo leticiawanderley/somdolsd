6be6f62d2fed1f3f4392d17c5293fc54c0871432
Leticia Wanderley <leticiafwanderley@gmail.com>
2017-05-06 10:10:54 -0300

    Cadastro atualizado

--- app/app/register/registerController.js
+++ app/app/register/registerController.js
@@ -3,12 +3,17 @@
 var somDoLSD = angular.module('somDoLSD');
 somDoLSD.controller('registerController', function(registerService) {
   var vm = this;
+  vm.user = {
+    username: '',
+    name: '',
+    sala: ''
+  }
   vm.salas = ['Sapupara', 'Volúpia',
   'Dona Bica', 'Bruxaxá', 'Rainha',
   'Auditório São Paulo', 'Serra Preta',
   'Triunfo', 'Carvalheira', 'Serra Limpa',
   'Engenho do Meio', 'Ypióca', 'Caruçu'];
   vm.add = function() {
-    registerService.addUsername(vm.username);
+    registerService.addUser(vm.user);
   }
 });
